3be67abd164b852ccb716c3e6c364d40ec9d54e8
Jaouad ER-RADI <jaouaderradi@proton.me>
2024-12-30 14:50:26 +0100

    Initialize project

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  // Fonctionnalités expérimentales activées
+  experimental: {
+    scanPageMeta: 'after-resolve',    // Optimisation des métadonnées après résolution des pages
+    sharedPrerenderData: false,       // Désactiver le partage des données de pré-rendu
+    compileTemplate: true,            // Activer la compilation des templates
+    resetAsyncDataToUndefined: true,  // Réinitialiser les données async à undefined
+    templateUtils: true,              // Activer les utilitaires de templates
+    relativeWatchPaths: true,         // Utiliser des chemins relatifs pour la surveillance
+    normalizeComponentNames: false,   // Ne pas normaliser les noms des composants
+    defaults: {
+      useAsyncData: {
+        deep: true, // Appliquer les paramètres par défaut de manière profonde
+      },
+    },
+  },
+
+  devtools: { enabled: true }
+})
